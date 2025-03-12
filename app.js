57b2625abb91e9428735cf7f467dc2ab61ef8647
Ray <rerrl5024@gmail.com>
2025-03-11 20:22:52 -0500

    Verificacion simple del boton agregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = []
+let listaAmigos = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    let verificaNombre = Array.from(nombre);
+    console.log(nombre);
+    console.log(verificaNombre);
+    if(nombre == " " || nombre == "" || verificaNombre < 2){
+        alert("Por favor, inserte un nombre");
+        limpiarCaja();       
+    }else{
+        limpiarCaja();
+        listaAmigos.push(nombre);
+    }
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
