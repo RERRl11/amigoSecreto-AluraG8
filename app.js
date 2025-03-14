eb5358b236198b7d8fa8d38d5914b4513d9c7716
Ray <rerrl5024@gmail.com>
2025-03-14 18:54:02 -0500

    Cambio de iconos y colores de botones. Boton de eliminar nombre añadido.

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ function agregarAmigo(){
     let nombre = document.getElementById("amigo").value;
 
     if(nombre.trim() === "" || nombre.trim().length <= 1){
-        alert("Por favor, inserte un nombre");
+        alert("Por favor, digite un nombre valido");
         limpiarCaja();       
     }else{
         if(listaAmigos.includes(nombre)){
             alert(`${nombre} ya ha sido ingresado. Digite otro nombre`)
+            limpiarCaja();
         }else{
             listaAmigos.push(nombre);
         }
@@ -24,22 +25,39 @@ function listadoAmigos(){
     for(let i = 0; i < listaAmigos.length ; i++){
         let amigos = listaAmigos[i];
         let itemLista = document.createElement("li");
-        itemLista.textContent = amigos;
+        itemLista.className = "item-lista";
+        itemLista.innerHTML = `<span>${amigos}</span> <button class="eliminar" data-indice="${i}"><img src="assets/usuario.png" alt="Eliminar"></button>`;
         lista.appendChild(itemLista);
     }
+    let botonesEliminar = document.querySelectorAll(".eliminar");
+    
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener("click", function() {
+            let indice = parseInt(this.getAttribute("data-indice"));
+            listaAmigos.splice(indice, 1);
+            listadoAmigos();
+        });
+    });
 }
 
 function sortearAmigo(){
-    if(listaAmigos.length > 1){
+    if(listaAmigos.length >= 1){
         let indiceAleatorio = Math.floor(Math.random()*listaAmigos.length);
-        let amigoSorteado = listaAmigos[indiceAleatorio];
+        let amigoSecreto = listaAmigos[indiceAleatorio];
         let sorteado = document.getElementById("resultado");
+        sorteado.innerHTML = "";
         let itemSorteado = document.createElement("li");
-        itemSorteado.textContent = amigoSorteado;
+        listaAmigos.splice(indiceAleatorio,1);
+        listadoAmigos();
+        itemSorteado.textContent = amigoSecreto;
         sorteado.appendChild(itemSorteado);
+        if(listaAmigos.length == 0){
+            alert("Todos los participantes fueron sorteados.");
+        }        
     }else{
-        alert("Ingrese dos o más nombres para sortear la lista")
+        alert("Ingrese uno o más nombres para sortear la lista")
     }
+    
 }
 
 function limpiarLista(){
